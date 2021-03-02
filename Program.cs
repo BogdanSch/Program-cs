1543a45684ca814d8530b5048a747d05f7df11be
BogdanSch <68709795+BogdanSch@users.noreply.github.com>
2021-03-02 16:09:35 +0200

    Update and rename README.md to Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+# Program-cs
+using System;
+
+namespace Program
+{
+    class Program-cs
+    {
+        private static void Main(string[] arts)
+        {
+             string str = "Hello";
+             Console.WriteLine(str);
+        }
+    }
+}
